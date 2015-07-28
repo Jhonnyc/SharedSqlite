52dbb29f874f54894f97c654a0f362ba33ff7d8a
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-07-28 13:06:55 +0300

    Added a method to read an array of strings and write an array of values

--- SharedSqlite/src/com/sharedsqlite/SharedSqlite.java
+++ SharedSqlite/src/com/sharedsqlite/SharedSqlite.java
@@ -18,37 +18,43 @@ public class SharedSqlite {
 
 	// Class Variables
 	private AtomicInteger mOpenCounter = new AtomicInteger();
-    private static SQLiteOpenHelper mDatabaseHelper;
+	private static SQLiteOpenHelper mDatabaseHelper;
 	private static SharedSqlite mInstance = null;
 	private SQLiteDatabase mDatabase;
-	
+
 	public static abstract class DatabaseEntry implements BaseColumns {
 		public static final String TABLE_COMMON_DATA = "shared_values_table";
 		public static final String COLUMN_DATA_KEY = "key";
 		public static final String COLUMN_DATA_VALUE = "value";
-    }
-	
+	}
+
 	/**
 	 * A static method to initialize a new instance of a SharedSqlite class
-	 * @param context A context object to create a new SharedSqlite class
+	 * 
+	 * @param context
+	 *            A context object to create a new SharedSqlite class
 	 */
 	public synchronized static void initialize(Context context) {
-		if(mInstance == null) {
+		if (mInstance == null) {
 			mInstance = new SharedSqlite();
 			mDatabaseHelper = new SharedSqliteDatabase(context);
 		}
 	}
-	
+
 	/**
 	 * A method used to get a new instance of the SharedSqlite class
-	 * @return A reference for the an instance of this class 
-	 * @throws NullPointerException In case the class has never been initialized
-	 * Use SharedSqlite.initialize(context) to create a new instance prior to this method
+	 * 
+	 * @return A reference for the an instance of this class
+	 * @throws NullPointerException
+	 *             In case the class has never been initialized Use
+	 *             SharedSqlite.initialize(context) to create a new instance
+	 *             prior to this method
 	 */
 	public static SharedSqlite getInstance() throws NullPointerException {
-		if(mInstance == null) {
-			throw new NullPointerException("The class has never been initialized. " +
-					"Use initialize(context) first to create a new instance");
+		if (mInstance == null) {
+			throw new NullPointerException(
+					"The class has never been initialized. "
+							+ "Use initialize(context) first to create a new instance");
 		}
 		return mInstance;
 	}
@@ -57,29 +63,29 @@ public class SharedSqlite {
 	 * Open the database for read and increment the counter of readers by one
 	 * And return a writable SQLiteDatabase object
 	 */
-    private synchronized SQLiteDatabase openDatabase() {
-        if(mOpenCounter.incrementAndGet() == 1) {
-            // Opening new database
-            mDatabase = mDatabaseHelper.getWritableDatabase();
-        }
-        return mDatabase;
-    }
+	private synchronized SQLiteDatabase openDatabase() {
+		if (mOpenCounter.incrementAndGet() == 1) {
+			// Opening new database
+			mDatabase = mDatabaseHelper.getWritableDatabase();
+		}
+		return mDatabase;
+	}
 
-    /*
-     * Close the database in case all the pointers to it are 'gone'
-     * that is openCounter value is 0
-     */
-    private synchronized void closeDatabase() {
-        if(mOpenCounter.decrementAndGet() == 0) {
-            // Closing database
-            mDatabase.close();
+	/*
+	 * Close the database in case all the pointers to it are 'gone' that is
+	 * openCounter value is 0
+	 */
+	private synchronized void closeDatabase() {
+		if (mOpenCounter.decrementAndGet() == 0) {
+			// Closing database
+			mDatabase.close();
 
-        }
-    }
+		}
+	}
 
-    /*
-	 * A private method used to add a row to the table in case it does not exists
-	 * Or update a row value in case it exist in the database
+	/*
+	 * A private method used to add a row to the table in case it does not
+	 * exists Or update a row value in case it exist in the database
 	 */
 	private boolean addOrUpdate(String dataKey, String value) {
 
@@ -92,27 +98,32 @@ public class SharedSqlite {
 		try {
 			db = getInstance().openDatabase();
 			row = new ContentValues();
-			if(getStringValue(dataKey, null) != null) {
+			if (getStringValue(dataKey, null) != null) {
 				row.put(DatabaseEntry.COLUMN_DATA_VALUE, value);
-				rowId = db.update(DatabaseEntry.TABLE_COMMON_DATA, row, DatabaseEntry.COLUMN_DATA_KEY + "='"+dataKey+"'", null);
-				if(rowId > -1) {
+				rowId = db.update(DatabaseEntry.TABLE_COMMON_DATA, row,
+						DatabaseEntry.COLUMN_DATA_KEY + "='" + dataKey + "'",
+						null);
+				if (rowId > -1) {
 					pass = true;
 				}
 			} else {
 				row.put(DatabaseEntry.COLUMN_DATA_KEY, dataKey);
 				row.put(DatabaseEntry.COLUMN_DATA_VALUE, value);
-				rowId = db.insertWithOnConflict(DatabaseEntry.TABLE_COMMON_DATA, null, row, SQLiteDatabase.CONFLICT_REPLACE);
-				if(rowId > -1) {
+				rowId = db.insertWithOnConflict(
+						DatabaseEntry.TABLE_COMMON_DATA, null, row,
+						SQLiteDatabase.CONFLICT_REPLACE);
+				if (rowId > -1) {
 					pass = true;
 				}
 			}
-//			row = new ContentValues();
-//			row.put(DatabaseEntry.COLUMN_DATA_KEY, dataKey);
-//			row.put(DatabaseEntry.COLUMN_DATA_VALUE, value);
-//			rowId = db.insertWithOnConflict(DatabaseEntry.TABLE_COMMON_DATA, null, row, SQLiteDatabase.CONFLICT_REPLACE);
-//			if(rowId > -1) {
-//				pass = true;
-//			}
+			// row = new ContentValues();
+			// row.put(DatabaseEntry.COLUMN_DATA_KEY, dataKey);
+			// row.put(DatabaseEntry.COLUMN_DATA_VALUE, value);
+			// rowId = db.insertWithOnConflict(DatabaseEntry.TABLE_COMMON_DATA,
+			// null, row, SQLiteDatabase.CONFLICT_REPLACE);
+			// if(rowId > -1) {
+			// pass = true;
+			// }
 		} catch (SQLException exception) {
 			Log.e(TAG, exception.getMessage());
 		} finally {
@@ -120,36 +131,64 @@ public class SharedSqlite {
 				if (db != null) {
 					// close database connection
 					getInstance().closeDatabase();
-				} 
+				}
 			} catch (SQLException exception) {
 				Log.e(TAG, exception.getMessage());
 			}
 		}
 		return pass;
 	}
-	
+
 	/**
-	 * A method to add a new row to the database. 
-	 * The data will be added as a key-value and the key will be 
-	 * used to fetch the data later on 
-	 * @param dataKey The key to be used later to fetch the data
-	 * @param value The data to be stored
+	 * A method to add a new row to the database. The data will be added as a
+	 * key-value and the key will be used to fetch the data later on
+	 * 
+	 * @param dataKey
+	 *            The key to be used later to fetch the data
+	 * @param value
+	 *            The data to be stored
 	 * @return True in case the row added successfully False otherwise
 	 */
 	public <K, V> boolean addValue(K dataKey, V value) {
-		boolean pass = addOrUpdate(String.valueOf(dataKey), String.valueOf(value));
+		boolean pass = addOrUpdate(String.valueOf(dataKey),
+				String.valueOf(value));
 		return pass;
 	}
-	
+
+	/**
+	 * A method to add a new row to the database. The data will be added as a
+	 * key-value and the key will be used to fetch the data later on
+	 * 
+	 * @param dataKey
+	 *            The key to be used later to fetch the data
+	 * @param values
+	 *            An array of data to be stored
+	 * @return True in case the row added successfully False otherwise
+	 */
+	public <K, V> boolean addValuesArray(K dataKey, V[] values) {
+		String value = "";
+		for (int i = 0; i < (values.length - 1); i++) {
+			value += values[i] + ",";
+		}
+		value += values[(values.length - 1)];
+
+		boolean pass = addOrUpdate(String.valueOf(dataKey),
+				String.valueOf(value));
+		return pass;
+	}
+
 	/**
 	 * Get a new String value by the given key
-	 * @param dataKey The key to look a new value by
-	 *
-	 * @param defaultValue A default return value, if there is no mapping 
-	 * For the given key this value will be returned
 	 * 
-	 * @return A String value associated with the given key or the defaultValue in 
-	 * Case there is no such mapping
+	 * @param dataKey
+	 *            The key to look a new value by
+	 * 
+	 * @param defaultValue
+	 *            A default return value, if there is no mapping For the given
+	 *            key this value will be returned
+	 * 
+	 * @return A String value associated with the given key or the defaultValue
+	 *         in Case there is no such mapping
 	 */
 	public <K> String getStringValue(K dataKey, String defaultValue) {
 		// method variables
@@ -162,11 +201,12 @@ public class SharedSqlite {
 		try {
 			db = getInstance().openDatabase();
 			cursor = db.query(DatabaseEntry.TABLE_COMMON_DATA, null,
-					DatabaseEntry.COLUMN_DATA_KEY + " = ?", new String[] { String.valueOf(dataKey) },
-					null, null, null);
+					DatabaseEntry.COLUMN_DATA_KEY + " = ?",
+					new String[] { String.valueOf(dataKey) }, null, null, null);
 			if (cursor != null) {
 				if (cursor.moveToFirst()) {
-					columnIndex = cursor.getColumnIndex(DatabaseEntry.COLUMN_DATA_VALUE);
+					columnIndex = cursor
+							.getColumnIndex(DatabaseEntry.COLUMN_DATA_VALUE);
 					if (columnIndex > -1) {
 						value = cursor.getString(columnIndex);
 					}
@@ -176,8 +216,8 @@ public class SharedSqlite {
 			Log.e(TAG, exception.getMessage());
 		} finally {
 			try {
-				if(cursor != null) {
-					if(!cursor.isClosed()) {
+				if (cursor != null) {
+					if (!cursor.isClosed()) {
 						cursor.close();
 					}
 				}
@@ -193,13 +233,16 @@ public class SharedSqlite {
 
 	/**
 	 * Get a new Integer value by the given key
-	 * @param dataKey The key to look a new value by
-	 *
-	 * @param defaultValue A default return value, if there is no mapping 
-	 * For the given key this value will be returned
 	 * 
-	 * @return A Integer value associated with the given key or the defaultValue in 
-	 * Case there is no such mapping
+	 * @param dataKey
+	 *            The key to look a new value by
+	 * 
+	 * @param defaultValue
+	 *            A default return value, if there is no mapping For the given
+	 *            key this value will be returned
+	 * 
+	 * @return A Integer value associated with the given key or the defaultValue
+	 *         in Case there is no such mapping
 	 */
 	public <K> Integer getIntValue(K dataKey, Integer defaultValue) {
 		Integer value = defaultValue;
@@ -207,20 +250,24 @@ public class SharedSqlite {
 		if (stringValue != null) {
 			try {
 				value = Integer.valueOf(stringValue);
-			} catch (Exception e) {	}
+			} catch (Exception e) {
+			}
 		}
 		return value;
 	}
-	
+
 	/**
 	 * Get a new Long value by the given key
-	 * @param dataKey The key to look a new value by
-	 *
-	 * @param defaultValue A default return value, if there is no mapping 
-	 * For the given key this value will be returned
 	 * 
-	 * @return A Long value associated with the given key or the defaultValue in 
-	 * Case there is no such mapping
+	 * @param dataKey
+	 *            The key to look a new value by
+	 * 
+	 * @param defaultValue
+	 *            A default return value, if there is no mapping For the given
+	 *            key this value will be returned
+	 * 
+	 * @return A Long value associated with the given key or the defaultValue in
+	 *         Case there is no such mapping
 	 */
 	public <K> Long getLongValue(K dataKey, Long defaultValue) {
 		Long value = defaultValue;
@@ -228,20 +275,24 @@ public class SharedSqlite {
 		if (stringValue != null) {
 			try {
 				value = Long.valueOf(stringValue);
-			} catch (Exception e) {	}
+			} catch (Exception e) {
+			}
 		}
 		return value;
 	}
-	
+
 	/**
 	 * Get a new Double value by the given key
-	 * @param dataKey The key to look a new value by
-	 *
-	 * @param defaultValue A default return value, if there is no mapping 
-	 * For the given key this value will be returned
 	 * 
-	 * @return A Double value associated with the given key or the defaultValue in 
-	 * Case there is no such mapping
+	 * @param dataKey
+	 *            The key to look a new value by
+	 * 
+	 * @param defaultValue
+	 *            A default return value, if there is no mapping For the given
+	 *            key this value will be returned
+	 * 
+	 * @return A Double value associated with the given key or the defaultValue
+	 *         in Case there is no such mapping
 	 */
 	public <K> Double getDoubleValue(K dataKey, Double defaultValue) {
 		Double value = defaultValue;
@@ -249,20 +300,24 @@ public class SharedSqlite {
 		if (stringValue != null) {
 			try {
 				value = Double.valueOf(stringValue);
-			} catch (Exception e) {	}
+			} catch (Exception e) {
+			}
 		}
 		return value;
 	}
-	
+
 	/**
 	 * Get a new Float value by the given key
-	 * @param dataKey The key to look a new value by
-	 *
-	 * @param defaultValue A default return value, if there is no mapping 
-	 * For the given key this value will be returned
 	 * 
-	 * @return A Float value associated with the given key or the defaultValue in 
-	 * Case there is no such mapping
+	 * @param dataKey
+	 *            The key to look a new value by
+	 * 
+	 * @param defaultValue
+	 *            A default return value, if there is no mapping For the given
+	 *            key this value will be returned
+	 * 
+	 * @return A Float value associated with the given key or the defaultValue
+	 *         in Case there is no such mapping
 	 */
 	public <K> Float getFloatValue(K dataKey, Float defaultValue) {
 		Float value = defaultValue;
@@ -270,20 +325,24 @@ public class SharedSqlite {
 		if (stringValue != null) {
 			try {
 				value = Float.valueOf(stringValue);
-			} catch (Exception e) {	}
+			} catch (Exception e) {
+			}
 		}
 		return value;
 	}
-	
+
 	/**
 	 * Get a new Boolean value by the given key
-	 * @param dataKey The key to look a new value by
-	 *
-	 * @param defaultValue A default return value, if there is no mapping 
-	 * For the given key this value will be returned
 	 * 
-	 * @return A Boolean value associated with the given key or the defaultValue in 
-	 * Case there is no such mapping
+	 * @param dataKey
+	 *            The key to look a new value by
+	 * 
+	 * @param defaultValue
+	 *            A default return value, if there is no mapping For the given
+	 *            key this value will be returned
+	 * 
+	 * @return A Boolean value associated with the given key or the defaultValue
+	 *         in Case there is no such mapping
 	 */
 	public <K> Boolean getBooleanValue(K dataKey, Boolean defaultValue) {
 		Boolean value = defaultValue;
@@ -291,13 +350,33 @@ public class SharedSqlite {
 		if (stringValue != null) {
 			try {
 				value = Boolean.valueOf(stringValue);
-			} catch(Exception e) { }
+			} catch (Exception e) {
+			}
 		}
 		return value;
 	}
-	
+
+	/**
+	 * A method to retrieve an array of strings by a key
+	 * 
+	 * @param dataKey
+	 *            The key to look a new value by
+	 * @return A String array of values associated with the given key or null in
+	 *         Case there is no such mapping
+	 */
+	public <K> String[] getStringArray(K dataKey) {
+		String[] values = null;
+		String value = getStringValue(dataKey, null);
+		if (value != null) {
+			values = value.split(",");
+		}
+
+		return values;
+	}
+
 	/**
 	 * A method to reset the database and clear any last saved value
+	 * 
 	 * @return True in case the entire data got erased
 	 */
 	public boolean reset() {
@@ -308,9 +387,11 @@ public class SharedSqlite {
 		SQLiteDatabase db = null;
 		try {
 			db = openDatabase();
-			cursor = db.query(DatabaseEntry.TABLE_COMMON_DATA, null, null, null, null, null, null);
+			cursor = db.query(DatabaseEntry.TABLE_COMMON_DATA, null, null,
+					null, null, null, null);
 			totalRows = cursor.getCount();
-			rowsDeleted = db.delete(DatabaseEntry.TABLE_COMMON_DATA, null, null);
+			rowsDeleted = db
+					.delete(DatabaseEntry.TABLE_COMMON_DATA, null, null);
 			pass = rowsDeleted == totalRows;
 		} catch (SQLException exception) {
 			pass = false;
